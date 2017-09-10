5f56da7882e35ebb40f72f2a0382ce915db929a5
Frantz Gary J <Gary.Frantz@dematic.com>
2017-09-10 08:44:44 -0500

    Added sample call to build ISS BD block

--- ISSBuilder/Logic.cs
+++ ISSBuilder/Logic.cs
@@ -10,12 +10,17 @@ namespace ISSBuilder
     /// <summary>
     /// Class to process the form and output a .L5X file.
     /// </summary>
+    /// <param name="merge"> Merge Object containing all information about the 
+    ///         to generate.</param>
     public class Logic
     {
         public static void ExportLogic(MergeModel merge)
         {
             //test to make sure this method executes.
             FA_ISS_C mc = new FA_ISS_C("MC", "UID", merge.NumLanes, merge.RecircID);
+            FA_ISS_BD bed = new FA_ISS_BD("BD", "UID");
+
+
         }
     }
 }
